4f17656a4dcbe4292f9a5911442ad5140944ffd7
Ben Burdette <bburdette@gmail.com>
2020-10-23 13:38:51 -0600

    add libglade-2.0 to include paths

--- shell.nix
+++ shell.nix
@@ -4,14 +4,20 @@ in
   with nixpkgs;
   stdenv.mkDerivation {
     name = "timekeeper-env";
+
+  
     buildInputs = [
       codeblocksFull
       poco
       pkgconfig
       gnome2.gtkmm
       gnome2.libglademm
+      gnome2.libglade
       # enable for editing *.glade files
       # glade
       nix
       ];
+
+  NIX_CFLAGS_COMPILE = "-isystem ${lib.getDev gnome2.libglade}/include/libglade-2.0";
+
   }
